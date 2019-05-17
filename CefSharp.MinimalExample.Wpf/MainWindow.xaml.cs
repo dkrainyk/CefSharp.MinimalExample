8fa4bd572240695f485d4323a5ca247b1eacbb0b
Dmitry Krainyk <d.kraynyk@gmail.com>
2019-05-17 23:52:19 +0200

    Demo 73.1 blank browser bug.

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,12 +1,25 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Windows;
 
 namespace CefSharp.MinimalExample.Wpf
 {
     public partial class MainWindow : Window
     {
+        public List<TabViewModel> Tabs { get; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Tabs = ConfigurationManager.AppSettings["tabs"]
+                .Split(new[] { '█' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => new TabViewModel(x, x))
+                .ToList();
+
+            DataContext = this;
         }
     }
 }
---
+++ CefSharp.MinimalExample.Wpf/TabViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Media;
+using CefSharp.Wpf;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    public class TabViewModel
+    {
+        public string Header { get; }
+
+        public ChromiumWebBrowser Browser { get; }
+
+        public TabViewModel(string header, string address)
+        {
+            Header = header;
+            Browser = new ChromiumWebBrowser(address)
+            {
+                BrowserSettings = { WebSecurity = CefState.Disabled },
+                Uid = Guid.NewGuid().ToString()
+            };
+            RenderOptions.SetBitmapScalingMode(Browser, BitmapScalingMode.NearestNeighbor);
+        }
+    }
+}
